9a088338c04bee3ae5ed856e2afa1e6b651b20b8
Miguel Perez <miguel250@gmail.com>
2011-05-29 02:16:17 -0400

    Added entity class

---
+++ Entity/Projects.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the MLPZ\ShortURLBundle
+ *
+ * (c) Miguel Perez <miguel@mlpz.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace WeekendHacker\ProjectsBundle\Entity;
+
+use WeekendHacker\ProjectsBundle\Model\Projects as AbstractProjects;
+
+/**
+ * Description of Projects
+ *
+ * @author Miguel Perez  <miguel@mlpz.com>
+ */
+
+abstract class Projects extends AbstractProjects
+{
+
+}
